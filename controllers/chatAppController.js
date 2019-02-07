5b69ca9b9270a315196f417f2674f295d8473d16
Yago Santos de Sousa <yagosansz@gmail.com>
2019-02-07 16:43:10 -0500

    added broadcast messages

--- controllers/chatAppController.js
+++ controllers/chatAppController.js
@@ -3,5 +3,16 @@ module.exports = function(io) {
   io.on('connect', function(socket) {
     // socket.id -> unique channel through which client and server can communicate
     console.log('made socket connection... ' + socket.id);
+    // listen to message being sent from the client
+    socket.on('chat', function(data) {
+      // send message to all different clients connect to the server on a websocket
+      // refers to all the socketS connected to the server
+      io.sockets.emit('chat', data);
+    });
+
+    socket.on('typing', function(data) {
+      // will emit to every single client, but not to the original one
+      socket.broadcast.emit('typing', data);
+    });
   });
 };
--- public/chat.js
+++ public/chat.js
@@ -1,2 +1,34 @@
 // make connection - frontend
 var socket = io.connect('http://localhost:3000');
+
+// query DOM
+var message = document.getElementById('message');
+var handle = document.getElementById('handle');
+var btn = document.getElementById('send');
+var output = document.getElementById('output');
+var feedback = document.getElementById('feedback');
+
+// emit events
+btn.addEventListener('click', function() {
+  // emit an event (message) that we can receive on the client
+  // send it through the socket down the webserver
+  socket.emit('chat', {
+    message: message.value,
+    handle: handle.value
+  });
+});
+
+message.addEventListener('keypress', function() {
+  socket.emit('typing', handle.value);
+});
+
+// listen for events on the frontend - data coming back from the server
+socket.on('chat', function(data) {
+  feedback.innerHTML = '';
+  output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+  message.value = '';
+});
+
+socket.on('typing', function(data) {
+  feedback.innerHTML = '<p><em>' + data + ' is typing a message...' + '</em></p>';
+});
